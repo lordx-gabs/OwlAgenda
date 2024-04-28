bbe8620dbc4d3844380a96352969b6f83590ce55
Gabriel de Proenca <bielsapata@gmail.com>
2024-04-27 21:03:39 -0300

    Layout melhorado, definição do temas claro e escuro.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -73,7 +73,7 @@ dependencies {
     implementation(platform("com.google.firebase:firebase-bom:32.8.1"))
     implementation("com.google.firebase:firebase-auth:22.3.1")
     implementation("com.google.firebase:firebase-storage:20.3.0")
-    implementation ("org.twitter4j:twitter4j-core:4.0.7")
+    implementation("com.google.android.material:material:1.11.0")
     implementation(libs.appcompat)
     implementation(libs.material)
     implementation(libs.activity)
