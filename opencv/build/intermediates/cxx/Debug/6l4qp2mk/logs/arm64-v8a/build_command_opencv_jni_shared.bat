5bfb536168a592ca58ffa4914f24199cb9c45315
Rafael-Meki7 <mekihub7@gmail.com>
2024-09-14 20:22:27 -0300

    Adicionamos a configuração de alteração de tema, além da tela também;

---
+++ opencv/build/intermediates/cxx/Debug/6l4qp2mk/logs/arm64-v8a/build_command_opencv_jni_shared.bat
@@ -0,0 +1,5 @@
+@echo off
+"C:\\Users\\PC\\AppData\\Local\\Android\\Sdk\\cmake\\3.22.1\\bin\\ninja.exe" ^
+  -C ^
+  "C:\\Users\\PC\\AndroidStudioProjects\\OwlAgenda\\opencv\\.cxx\\Debug\\6l4qp2mk\\arm64-v8a" ^
+  opencv_jni_shared
