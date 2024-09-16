6a7cf7bd36c9697a90dcb0d305338c4691d1edf1
Gabriel de Proenca <bielsapata@gmail.com>
2024-09-16 12:11:48 -0300

    commit necessario para dar um merge nas branch.

---
+++ opencv/build/intermediates/cxx/Debug/6l4qp2mk/logs/x86/build_command_opencv_jni_shared.bat
@@ -0,0 +1,5 @@
+@echo off
+"C:\\Users\\biels\\AppData\\Local\\Android\\Sdk\\cmake\\3.22.1\\bin\\ninja.exe" ^
+  -C ^
+  "C:\\Users\\biels\\AndroidStudioProjects\\teste7\\opencv\\.cxx\\Debug\\6l4qp2mk\\x86" ^
+  opencv_jni_shared
