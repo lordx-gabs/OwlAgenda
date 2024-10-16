aa9f83d2acb6cb09605265d3e3dc62ec6db59bdc
lordx-gabs <bielsapata@gmail.com>
2024-10-16 08:43:58 -0300

    tela do gui

---
+++ opencv/build/intermediates/cxx/Debug/6l4qp2mk/logs/x86/configure_command.bat
@@ -0,0 +1,19 @@
+@echo off
+"C:\\Users\\userlocal\\AppData\\Local\\Android\\Sdk\\cmake\\3.22.1\\bin\\cmake.exe" ^
+  "-HC:\\Users\\userlocal\\AndroidStudioProjects\\OwlAgenda\\opencv\\libcxx_helper" ^
+  "-DCMAKE_SYSTEM_NAME=Android" ^
+  "-DCMAKE_EXPORT_COMPILE_COMMANDS=ON" ^
+  "-DCMAKE_SYSTEM_VERSION=26" ^
+  "-DANDROID_PLATFORM=android-26" ^
+  "-DANDROID_ABI=x86" ^
+  "-DCMAKE_ANDROID_ARCH_ABI=x86" ^
+  "-DANDROID_NDK=C:\\Users\\userlocal\\AppData\\Local\\Android\\Sdk\\ndk\\26.1.10909125" ^
+  "-DCMAKE_ANDROID_NDK=C:\\Users\\userlocal\\AppData\\Local\\Android\\Sdk\\ndk\\26.1.10909125" ^
+  "-DCMAKE_TOOLCHAIN_FILE=C:\\Users\\userlocal\\AppData\\Local\\Android\\Sdk\\ndk\\26.1.10909125\\build\\cmake\\android.toolchain.cmake" ^
+  "-DCMAKE_MAKE_PROGRAM=C:\\Users\\userlocal\\AppData\\Local\\Android\\Sdk\\cmake\\3.22.1\\bin\\ninja.exe" ^
+  "-DCMAKE_LIBRARY_OUTPUT_DIRECTORY=C:\\Users\\userlocal\\AndroidStudioProjects\\OwlAgenda\\opencv\\build\\intermediates\\cxx\\Debug\\6l4qp2mk\\obj\\x86" ^
+  "-DCMAKE_RUNTIME_OUTPUT_DIRECTORY=C:\\Users\\userlocal\\AndroidStudioProjects\\OwlAgenda\\opencv\\build\\intermediates\\cxx\\Debug\\6l4qp2mk\\obj\\x86" ^
+  "-DCMAKE_BUILD_TYPE=Debug" ^
+  "-BC:\\Users\\userlocal\\AndroidStudioProjects\\OwlAgenda\\opencv\\.cxx\\Debug\\6l4qp2mk\\x86" ^
+  -GNinja ^
+  "-DANDROID_STL=c++_shared"
