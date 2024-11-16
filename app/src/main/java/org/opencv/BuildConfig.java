2e347d4bc34e50cbc615a8c8ff9e95d36e733a62
Rafael-Meki7 <mekihub7@gmail.com>
2024-11-16 16:00:32 -0300

    Foto do desenvolvedor 2 adicionada (ainda consertar)

--- app/src/main/java/org/opencv/BuildConfig.java
+++ app/src/main/java/org/opencv/BuildConfig.java
@@ -7,4 +7,5 @@ public final class BuildConfig {
   public static final boolean DEBUG = Boolean.parseBoolean("true");
   public static final String LIBRARY_PACKAGE_NAME = "org.opencv";
   public static final String BUILD_TYPE = "debug";
+  public static String apiKeyGemini;
 }
