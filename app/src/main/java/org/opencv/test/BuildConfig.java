008794fc0ece5df9824f7f9e3b491ff25adcd47e
Rafael-Meki7 <mekihub7@gmail.com>
2024-11-16 15:34:24 -0300

    primeira versão fruto do commit do dia 15/11/2024 da main

--- app/src/main/java/org/opencv/test/BuildConfig.java
+++ app/src/main/java/org/opencv/test/BuildConfig.java
@@ -7,4 +7,5 @@ public final class BuildConfig {
   public static final boolean DEBUG = Boolean.parseBoolean("true");
   public static final String APPLICATION_ID = "org.opencv.test";
   public static final String BUILD_TYPE = "debug";
+  public static String tokenGoogle;
 }
